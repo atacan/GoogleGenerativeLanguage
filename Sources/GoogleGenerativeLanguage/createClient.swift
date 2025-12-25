d9df2d2f57164d292aa5c2127247b6d7ed6d43d2
atacan <atacandur@icloud.com>
2025-12-25 13:44:13 +0100

    Enhance createClient function to support customizable timeout for AsyncHTTPClientTransport

--- Sources/GoogleGenerativeLanguage/createClient.swift
+++ Sources/GoogleGenerativeLanguage/createClient.swift
@@ -1,13 +1,14 @@
 import OpenAPIAsyncHTTPClient
 import OpenAPIRuntime
+import NIOCore
 
-public func createClient(apiKey: String) throws -> Client {
+public func createClient(apiKey: String, timeout: TimeAmount = .minutes(20)) throws -> Client {
 
     let serverURL = try Servers.Server1.url()
     let client = Client(
         serverURL: serverURL,
         configuration: .init(dateTranscoder: .iso8601WithFractionalSeconds),
-        transport: AsyncHTTPClientTransport(),
+        transport: AsyncHTTPClientTransport(configuration: AsyncHTTPClientTransport.Configuration(timeout: timeout)),
         middlewares: [
             AuthenticationMiddleware(apiKey: apiKey),
             UnescapeGoogUploadHeadersMiddleware(),
